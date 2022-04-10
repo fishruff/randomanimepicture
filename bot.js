eb06f9e0b4b1cfa7f5ac85e0e565792f2677a12d
Ars <arsershoff@gmail.com>
2022-04-10 21:05:25 +0300

    fixed to stable version

--- bot.js
+++ bot.js
@@ -23,71 +23,18 @@ bot.hears("Дай тян", (ctx) => {
   );
   ctx.replyWithPhoto(url);
 });
-// Button 2
-// bot.hears("Ручной ввод чисel", (ctx) => {
-//   bot.enter(async (ctx) =>  await ctx.reply("Pls write psi (from 0.3 to 2.0)"))
-//   bot.on('text', async (ctx) => {
-//     let psi = Number(ctx.message.text)
-//       if(psi && psi >= 0.3 && psi <= 2.0)
-//       await ctx.reply('Thx for psi')
-//   })
-
-//   const url = getUrl(
-
-//   );
-//   ctx.replyWithPhoto(url);
-// });
-
-const inline_keyboard = Markup.inlineKeyboard(
-  [
-    Markup.callbackButton("Yes, send a keyboard", "yes"),
-    Markup.callbackButton("No", "no"),
-  ],
-  {
-    columns: 1,
-  }
-);
-
-const keyboard = Markup.keyboard([["Top"], ["Button 1", "Button 2"]]);
-
-bot.action("no", (ctx) => ctx.answerCbQuery("No so no"));
-bot.action("yes", async (ctx) => {
-  await ctx.answerCbQuery("Okey");
-
-  await ctx.deleteMessage(ctx.callbackQuery.message.message_id);
-
-  await ctx.reply("Enter Psi", keyboard.resize().extra());
-});
 
 bot.start(async (ctx) => {
-  ctx.reply(
-    "Hello!",
-    inline_keyboard.extra()
-    //  {
-    //   reply_markup: {
-    //     keyboard: [[{ text: "Дай тян" },
-    //     //  { text: "Ручной ввод чисel" }
-    //     ]],
-    //     resize_keyboard: true,
-    //     т,
-    //   },
-    // }
-  );
+  ctx.reply("Hello!", {
+    reply_markup: {
+      keyboard: [[{ text: "Дай тян" }]],
+      resize_keyboard: true,
+    },
+  });
 });
-
-// const stage = new Stage([psiScene, seedScene]);
-// bot.use(session());
-// bot.use(stage.middleware());
-
-// bot.command("scenes", async (ctx) => {
-//   ctx.scene.enter("psi");
-// });
-bot.help((ctx) => ctx.reply("wqeqwe")); //ответ бота на команду /help
-bot.on("sticker", (ctx) => ctx.reply("")); //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
-// bot.hears("hi", (ctx) => ctx.reply("Hey there")); // bot.hears это обработчик конкретного текста, данном случае это - "hi"
 
 bot.launch().then((e) => {
   console.log("started");
-}); // запуск бота
+});
 
 const random = (start = 0, end = 100) => Math.random() * (end - start) + start;
